a29a48a574ffb94d9a1f304b006d06b6722db58d
raphplt <raphael.plassart@gmail.com>
2025-07-23 12:15:03 +0200

    fix(api): revert path.join usage in data source configuration for entity and migration paths

--- apps/api/src/data-source.ts
+++ apps/api/src/data-source.ts
@@ -1,14 +1,11 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import path from 'path';
-
 export const dataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: true,
-  entities: [path.join(__dirname, '**/*.entity.{js,ts}')],
-  migrations: [path.join(__dirname, 'migrations/*.{js,ts}')],
+  entities: [__dirname + '/**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/migrations/*.{js,ts}'],
 });
-
 export default dataSource;
