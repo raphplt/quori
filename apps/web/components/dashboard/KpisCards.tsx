7d60bd0d2760540f54675051bb5f766e70aa1a4e
raphplt <raphael.plassart@gmail.com>
2025-08-02 16:06:44 +0200

    clean commentaires

--- apps/web/components/dashboard/KpisCards.tsx
+++ apps/web/components/dashboard/KpisCards.tsx
@@ -41,7 +41,7 @@ const KpisCards = () => {
         </CardContent>
       </Card>
 
-      {/* Engagement Rate (Coming Soon) */}
+      {/* Engagement Rate */}
       <Card className="relative">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
@@ -61,7 +61,7 @@ const KpisCards = () => {
         </div>
       </Card>
 
-      {/* Total Impressions (Coming Soon) */}
+      {/* Total Impressions */}
       <Card className="relative">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
